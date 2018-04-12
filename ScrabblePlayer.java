ca787018d1b134cef84c3f7713bb24aca6b20778
CameronPH <37255169+CameronPH@users.noreply.github.com>
2018-04-12 15:34:07 -0400

    ScrabblePlayer_CH_V1

--- ScrabblePlayer.java
+++ ScrabblePlayer.java
@@ -20,17 +20,78 @@ import java.util.*;
 public class ScrabblePlayer
 {
     //Global fields
-    ArrayList<String> dictionary;
+    Node root;
+    
     
     // initialize ScrabblePlayer with a file of English words
     public ScrabblePlayer(String wordFile) throws FileNotFoundException
     {
         //Read in the word file and store the created dictionary in the "dictionary" global field
         Scanner dictFile = new Scanner(new File(wordFile));
-        dictionary = new ArrayList<>();
+        root = new Node(' ', null);
         while (dictFile.hasNext()) {
-            dictionary.add(dictFile.nextLine().toUpperCase());
+            ArrayList<Node> children = root.getChildren();
+            String nextWord = dictFile.nextLine().toUpperCase();
+            for (int charCtr = 0; charCtr < nextWord.length(); charCtr++) {
+                //Nothing has been added to the root
+                if (root.getChildren() == null) {
+                    root.appendChild(new Node(nextWord.charAt(0), nextWord));
+                } else {
+                    //Chilren of root is not equal to null
+                    boolean done = false;
+                    
+                    
+                    //Bolean flag to see if the char is already a child
+                    boolean doesExist = false;
+                    for (int childCtr = 0; childCtr < children.size(); childCtr++) {
+                        if (nextWord.charAt(charCtr) == children.get(childCtr).getLetter()) {
+                            if (charCtr == (nextWord.length() - 1)) {
+                                
+                                children.get(childCtr).validWord = nextWord;
+                            }
+                            children = children.get(childCtr).getChildren();
+                            doesExist = true;
+                            break;
+                        }
+                    }
+                    
+                    //If the character is not a child yet, add it
+                    if (!doesExist) {
+                        if (charCtr == (nextWord.length() - 1)) {
+                            children.add(new Node (nextWord.charAt(charCtr), nextWord));
+                        } else {
+                            children.add(new Node (nextWord.charAt(charCtr), null));
+                        }
+                        
+                        //Get the children of the node that was just added
+                        children = children.get(children.size()-1).getChildren();
+                        
+                    }
+                        
+                        
+
+                    
+                }
+            }
+            
         }
+        //************************************************************************CHANGES********************************
+        ArrayList<Node> children = root.getChildren().get(25).getChildren().get(0).getChildren();
+        for (Node e : children) {
+            System.out.println(e.getWord());
+        }
+        /*
+         * From the pdf file: "The constructor allows you to process the data and construct data structures in preparation for identifying words"
+         * I belive our goal is to build data structures in the constructor in under 5 minutes, that will then allow us to generate the best
+         * word in under 1 second.
+         */
+        
+        
+        
+        
+        
+        
+        
     }
 
     // based on the board and available letters, 
@@ -50,12 +111,124 @@ public class ScrabblePlayer
 
     public ScrabbleWord getScrabbleWord(char[][] board, char[] availableLetters)
     {
-
-
-
-
+        /*
+         * CH:
+         *  the board that is passed in has a random word from words.txt placed in a
+         *  random position on the board. So scan through the board to determine the location,
+         *  length, and orientation of the first word.
+         */
+        
+        //Will print the board if uncommented, it will show you where the word is in the board and
+        //what the orientation, start pos, and end pos should be.
+        /*
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                System.out.print(board[row][col]);
+            }
+            System.out.println();
+        }
+        */
+        
+        /*
+         * lines 76 - 101
+         * determines what and the opponent's word is, the orientation, and where it is located on the board
+         * Then it creates that words as a ScrabbleWord object to hold all that information.
+         */
+        String opponentWord = "";
+        char opponentOrientation = 'n';
+        int startRow = -1;
+        int startCol = -1;
+        //Iterate through the rows of the board
+        for (int row = 0; row < board.length; row++) {
+            //Iterate through the cols of each row in the board
+            for (int col = 0; col < board[0].length; col++) {
+                //If the current character is not a letter ********(this was confusing, don't question it, it works)********
+                if (Character.getNumericValue(board[row][col]) != -1) {
+                    opponentWord = opponentWord + board[row][col];
+                    
+                    //If the word is empty, determine the orientation
+                    if (opponentWord.length() == 1) {
+                        startRow = row;
+                        startCol = col;
+                        //Accounts for if word is on the border
+                        if (((row + 1) < board.length) && (Character.getNumericValue(board[row + 1][col]) != -1)) {
+                            opponentOrientation = 'v';
+                        } else if (((col + 1) < board[0].length) && (Character.getNumericValue(board[row][col + 1]) != -1)) {
+                            opponentOrientation = 'h';
+                        }
+                    }
+                }
+            }
+        }
+        ScrabbleWord opponent = new ScrabbleWord(opponentWord, startRow, startCol, opponentOrientation);
+        //System.out.println(opponent.getScrabbleWord());
+        //Potential method for choosing the longest word we can create
+        String longestWord = getLongestWordPossible(availableLetters);
+        
 
         return  new ScrabbleWord("MYWORD", 0, 0, 'h');
     }
 
+    /*
+     * Description:
+     *  getLongestWordPossible will take the available letters, and go through the dictionary to determine
+     *  the longest word we can could create that still conforms to the rules laid out in the assignment.
+     */
+    String getLongestWordPossible(char[] availableLetters) {
+        
+        
+        
+        
+        return "TESTING";
+    }
+    
+    static class Node {
+        String validWord;
+        char letter;
+        ArrayList<Node> children;
+        
+        Node(char newLetter, String newValidWord) {
+            validWord = newValidWord;
+            letter = newLetter;
+            children = new ArrayList<Node>();
+        }
+        
+        String getWord() {
+            return validWord;
+        }
+        
+        char getLetter() {
+            return letter;
+        }
+        
+        ArrayList<Node> getChildren() {
+            return children;
+        }
+        
+        void appendChild(Node newChild) {
+            children.add(newChild);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
